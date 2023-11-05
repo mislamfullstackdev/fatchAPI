38e51c4c4b7bdab868a39925b73d7783e48226f8
mislamfullstackdev <mislam.pasona@gmail.com>
2023-11-06 02:27:13 +0900

    Using XMLHttpRequest

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-console.log("index.js: loaded");
-const UserId = "themeexpress"
-fetch(`https://api.github.com/users/${encodeURIComponent(UserId)}`)
-    .then(response =>{
-        console.log(response.status);//check the status
-        if (!response.ok){
-            console.log("Bad request check the credinticals", response)
+function fetchGitUserInfo(userId){
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response =>{
+            console.log(response.status);//check the status
+            if (!response.ok){
+                console.log("Bad request check the credinticals", response)
+            }else{
+                return response.json().then(userInfo =>{
+                    //JSON will be goes here
+                    console.log(userInfo);
+                });
+            }
+        }).catch(error =>{
+            console.log(error)
+    });
+}
+
+function fetchByXmlHttpRequest(userId){
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://api.github.com/users/${encodeURIComponent(userId)}`);
+    request.addEventListener("load", () => {
+        // Fetch API Response OK or not
+        if(request.status >= 200 && request.status < 300){
+            const userInfo = JSON.parse(request.responseText);
+            console.log(userInfo);
         }else{
-            return response.json().then(userInfo =>{
-                //JSON will be goes here
-                console.log(userInfo);
-            });
+            console.error("Error ocurs", request.statusText);
         }
-    }).catch(error =>{
-        console.log(error)
-});
+    });
+    request.addEventListener("error", () => {
+        console.error("host error")
+    })
+    // finally send the request
+    request.send();
+}
