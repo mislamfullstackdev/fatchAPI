78e73bc24649d483b7fb5aa877719b343581a113
mislamfullstackdev <mislam.pasona@gmail.com>
2023-11-06 02:02:29 +0900

    Better Error handling
    Refactoring of Error handling

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ console.log("index.js: loaded");
 const UserId = "themeexpress"
 fetch(`https://api.github.com/users/${encodeURIComponent(UserId)}`)
     .then(response =>{
-        console.log(response.status);//check the result
-        return response.json().then(userInfo =>{
-            //JSON will be goes here
-            console.log(userInfo);
-        });
+        console.log(response.status);//check the status
+        if (!response.ok){
+            console.log("Bad request check the credinticals", response)
+        }else{
+            return response.json().then(userInfo =>{
+                //JSON will be goes here
+                console.log(userInfo);
+            });
+        }
     }).catch(error =>{
         console.log(error)
-    });
+});
