eda08d7fd4f6eb1d6b6671ababc405c6219a4c64
mislamfullstackdev <mislam.pasona@gmail.com>
2023-11-07 00:24:09 +0900

    Refactoring with promise and exception handling

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-function fetchGitUserInfo(userId){
-    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-        .then(response =>{
-            console.log(response.status);//check the status
-            if (!response.ok){
-                console.log("Bad request check the credinticals", response)
-            }else{
-                return response.json().then(userInfo =>{
-                    //console.log(userInfo);
-                    const view =  escapeHTML`
-                    <h4>${userInfo.name} (@${userInfo.login})</h4>
-                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-                    <dl>
-                        <dt>Location</dt>
-                        <dd>${userInfo.location}</dd>
-                        <dt>Repositories</dt>
-                        <dd>${userInfo.public_repos}</dd>
-                    </dl>
-                    `;
-                    // attached in HTML 
-                    const result = document.getElementById("result");
-                    result.innerHTML = view;
-                });
-            }
-        }).catch(error =>{
-            console.log(error)
+function main(){
+    fetchGitUserInfo("themeexpress")
+    .then((userInfo) => createView(userInfo))
+    .then((view) => displayView(view))
+    .catch((error) => {
+        // Promise
+        console.log(`Unexpected error occured ({$error})`);
     });
 }
 
+function fetchGitUserInfo(userId){
+    return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response =>{
+            if (!response.ok){
+                // Add promise
+                return Promise.reject(new Error(`${response.status}: ${response.statusText}`))
+            }else{
+                return response.json();
+            }
+    });
+}
+
+function createView(userInfo){
+    return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Repositories</dt>
+        <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
+}
+
+function displayView(view){
+    const result = document.getElementById("result")
+    result.innerHTML = view;
+}
 
 // escape special character and html
 function escapeSpecialChars(str) {
